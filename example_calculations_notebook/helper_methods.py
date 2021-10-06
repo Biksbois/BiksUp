79979bdbbe24bc0302c7911e7ec2df4d2f601fac
KuskIV <madshkusk@gmail.com>
2021-10-06 16:14:23 +0200

    jupyter notebook

---
+++ example_calculations_notebook/helper_methods.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def sigmoid(num):
+    return 1 / (1+np.exp(-num))
+
+def activation_function_on_vector(func, input_vector):
+    return [func(i) for i in input_vector]
+
+def tanh(x):
+    t=(np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+    dt=1-t**2
+    return t #,dt
