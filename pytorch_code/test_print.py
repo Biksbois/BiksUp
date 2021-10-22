6d7df6876aa0ebf8029bcfb0d646655aac49898f
KuskIV <madshkusk@gmail.com>
2021-10-22 12:59:38 +0200

    cleanup

--- pytorch_code/test_print.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.nn import Parameter, Module
-
-def print_parameter():
-    for i in range(5):
-        # print(Parameter(torch.Tensor(10,10)))
-        print(torch.Tensor(2,2) * 100)
