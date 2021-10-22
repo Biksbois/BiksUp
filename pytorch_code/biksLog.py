770ce65d7c268a38c4d529dfa5171abfecc5cf5d
KuskIV <madshkusk@gmail.com>
2021-10-22 13:11:06 +0200

    improved logger

--- pytorch_code/biksLog.py
+++ pytorch_code/biksLog.py
@@ -1,15 +1,37 @@
 import logging
 from rich.logging import RichHandler
+import sys
 
+APP_LOGGER_NAME = 'BiksUP'
 
 
 def get_logger():
-    logging.basicConfig(
-        level="NOTSET",
-        format="%(message)s",
-        datefmt="[%X]",
-        handlers=[RichHandler(rich_tracebacks=True)]
-    )
+    return setup_applevel_logger(file_name='logfile.log')
+    # logging.basicConfig(
+    #     level="NOTSET",
+    #     format="%(message)s",
+    #     datefmt="[%X]",
+    #     handlers=[RichHandler(rich_tracebacks=True)]
+    # )
 
-    log = logging.getLogger("rich")
-    return log
+    # log = logging.getLogger("rich")
+    # return log
+
+def setup_applevel_logger(logger_name = APP_LOGGER_NAME, file_name=None): 
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(formatter)
+    
+    logger.handlers.clear()
+    logger.addHandler(RichHandler(rich_tracebacks=True))
+    
+    if file_name:
+        fh = logging.FileHandler(file_name)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+    
+    return logger
